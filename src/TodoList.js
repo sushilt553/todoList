3a0e2897f702f057c87c79af9ce752c1640cd56a
Sushil <sushilt553@gmail.com>
2020-06-06 15:58:37 -0400

    React component

--- src/TodoList.js
+++ src/TodoList.js
@@ -3,21 +3,33 @@ import React from 'react';
 class TodoList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {items: []};
+        this.state = {items: [], inputVal: ""};
         this.addItem = this.addItem.bind(this);
+    }
+
+    update() {
+        return e => this.setState({inputVal: e.currentTarget.value})
     }
 
     addItem(e) {
         e.preventDefault();
-        
+        let newItem = [this.state.inputVal];
+        this.setState((prevState) => {
+            return {
+                items: prevState.items.concat(newItem)
+            }
+        });
+        this.setState({inputVal: ""});
     }
 
     render() {
         return (
-            <form onSubmit={this.addItem}>
-                <input type="text" placeholder="Enter a task" />
-                <button type="submit">Add</button>
-            </form>
+            <div>
+                <form onSubmit={this.addItem}>
+                    <input type="text" placeholder="Enter a task" onChange={this.update()} value={this.state.inputVal}/>
+                    <button type="submit">Add</button>
+                </form>
+            </div>
         )
     }
 
