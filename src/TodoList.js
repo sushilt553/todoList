a8f634e2d307d565a25da724ec7eaa681175c7ea
Sushil <sushilt553@gmail.com>
2020-06-06 16:02:55 -0400

    Todolist complete

--- src/TodoList.js
+++ src/TodoList.js
@@ -7,7 +7,7 @@ class TodoList extends React.Component {
         this.addItem = this.addItem.bind(this);
     }
 
-    update() {
+    update(e) {
         return e => this.setState({inputVal: e.currentTarget.value})
     }
 
@@ -23,12 +23,16 @@ class TodoList extends React.Component {
     }
 
     render() {
+        let list = this.state.items.map((ele, i) => <li key={i}>{ele}</li>)
         return (
             <div>
                 <form onSubmit={this.addItem}>
                     <input type="text" placeholder="Enter a task" onChange={this.update()} value={this.state.inputVal}/>
                     <button type="submit">Add</button>
                 </form>
+                <ul>
+                    {list}
+                </ul>
             </div>
         )
     }
